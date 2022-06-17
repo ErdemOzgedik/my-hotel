574c54da901055622a8d5c99b5c302a1a0bc3ae6
ErdemOzgedik <erdemozgedik@gmail.com>
2022-06-17 14:12:03 +0300

    react router added for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Add from "./pages/Add/Add";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="app">
-      {/* Router ekle */}
-
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="add" element={<Add />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
